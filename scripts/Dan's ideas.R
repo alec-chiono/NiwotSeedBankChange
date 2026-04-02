f82f85abaea5d77e70be7fc39fb7b2da0aab7f14
Alec Chiono <alec.chiono@colorado.edu>
2026-04-02 11:00:12 -0600

    Add Nancy's plot idea

--- scripts/Dan's ideas.R
+++ scripts/Dan's ideas.R
@@ -1,43 +1,91 @@
 
-librarian::shelf(tidyverse, ggrepel)
+librarian::shelf(tidyverse, ggbreak, ggthemes, patchwork)
 
 # DATA -------------------------------------------------------------------------
 ## Download
 source("scripts/source/download_data.R")
 
-theme_set(ggthemes::theme_tufte() + theme(panel.border=element_rect(fill=NA)))
+plot_df <- data_list$seedbank_composition.ac_hh.data %>%
+  filter(substr(USDA_code, 1, 1) != 2 & USDA_code != "CAREX" & USDA_code != "POA") %>%
+  mutate(Species = sub("\\s*var\\..*$", "", USDA_name),
+         year = factor(year)) %>%
+  group_by(year, Species) %>%
+  summarize(sum = sum(count), .groups = "drop")
 
-## Plot
-data_list$seedbank_composition.ac_hh.data %>%
-  filter(substr(USDA_code,1, 1)!=2 & USDA_code!="CAREX" & USDA_code!="POA") %>%  #remove records not identified to species
-  group_by(year, USDA_name) %>%
-  summarize(sum=sum(count), .groups="drop") %>%
-  pivot_wider(id_cols=USDA_name, names_from=year, values_from=sum) %>%
-  ggplot(aes(x=`1989`, y=`2023`, color=USDA_name)) +
-  geom_hline(yintercept=0, linetype=2, color="grey") +
-  geom_vline(xintercept=0, linetype=2, color="grey") +
-  geom_point() +
-  geom_text_repel(aes(label=USDA_name), size=3, max.overlaps=100, box.padding=.5, force=100, max.time=20) +
-  scale_x_continuous(name="Seeds in 1989", limits=c(-60, NA), breaks=c(0, 25, 50, 75)) +
-  scale_y_continuous(name="Seeds in 2023", limits=c(-10, NA), breaks=c(0, 25, 50, 75, 100)) +
-  theme(legend.position="none")
+species_order <- plot_df %>%
+  filter(year == "1989") %>%
+  arrange(desc(sum)) %>%
+  pull(Species)
 
+plot_df %>%
+  mutate(Species = factor(Species, levels = species_order)) %>%
+  ggplot(aes(x = Species, y = sum, color = year, fill = year)) +
+  geom_bar(stat = "identity", position = "dodge") +
+  scale_y_continuous(name = "# Germinable Seeds") +
+  scale_fill_manual(name = "Year", values = c("skyblue3", "red4"),
+                    aesthetics = c("fill", "color")) +
+  scale_y_break(c(21, 84), scales = 0.25) +
+  ggthemes::theme_tufte() +
+  theme(
+    axis.text.x = element_text(angle = 45, hjust = 1, face = "italic"),
+    axis.text.y.right = element_blank(),
+    axis.ticks.y.right = element_blank(),
+    axis.line.y.right = element_blank(),
+    axis.line.x.bottom = element_line(color = "black", linewidth = 0.4),
+    axis.line.y.left   = element_line(color = "black", linewidth = 0.4),
+    axis.ticks.y.left  = element_line(color = "black"),
+    strip.text = element_blank(),
+    legend.position = "top"
+  ) +
+  facet_wrap(~Species, nrow = 1, scales = "free_x")
 
 ## Plot by habitat
-data_list$seedbank_composition.ac_hh.data %>%
-  filter(substr(USDA_code,1, 1)!=2 & USDA_code!="CAREX" & USDA_code!="POA") %>%  #remove records not identified to species
-  group_by(year, habitat, USDA_name) %>%
-  summarize(sum=sum(count), .groups="drop") %>%
-  pivot_wider(id_cols=c(USDA_name, habitat), names_from=year, values_from=sum) %>%
-  ggplot(aes(x=`1989`, y=`2023`, color=USDA_name)) +
-  geom_hline(yintercept=0, linetype=2, color="grey") +
-  geom_vline(xintercept=0, linetype=2, color="grey") +
-  geom_point() +
-  geom_text_repel(aes(label=USDA_name), size=3, max.overlaps=100, box.padding=.5, force=100, max.time=20) +
-  scale_x_continuous(name="Seeds in 1989", limits=c(-60, NA), breaks=c(0, 25, 50, 75)) +
-  scale_y_continuous(name="Seeds in 2023", limits=c(-10, NA), breaks=c(0, 25, 50, 75, 100)) +
-  facet_grid(~habitat) +
-  theme(legend.position="none")
+hab_df <- data_list$seedbank_composition.ac_hh.data %>%
+  filter(substr(USDA_code, 1, 1) != 2 & USDA_code != "CAREX" & USDA_code != "POA") %>%
+  mutate(
+    Species = sub("\\s*var\\..*$", "", USDA_name),
+    year = factor(year)
+  ) %>%
+  group_by(year, habitat, Species) %>%
+  summarize(sum = sum(count), .groups = "drop")
+
+make_hab_plot <- function(dat, hab, show_x = TRUE) {
+  hab_order <- dat %>%
+    filter(habitat == hab, year == "1989") %>%
+    arrange(desc(sum)) %>%
+    pull(Species)
+
+  dat %>%
+    filter(habitat == hab) %>%
+    mutate(Species = factor(Species, levels = hab_order)) %>%
+    ggplot(aes(x = Species, y = sum, fill = year, color = year)) +
+    geom_col(position = "dodge") +
+    coord_cartesian(ylim = c(0, 25)) +
+    scale_y_continuous(name = "# Germinable Seeds") +
+    scale_fill_manual(
+      name = "Year",
+      values = c("skyblue3", "red4"),
+      aesthetics = c("fill", "color")
+    ) +
+    labs(title = hab, x = if (show_x) "Species" else NULL) +
+    ggthemes::theme_tufte() +
+    theme(
+      plot.title = element_text(hjust = 0, face = "bold"),
+      axis.text.x = if (show_x) element_text(angle = 45, hjust = 1, face = "italic") else element_blank(),
+      axis.ticks.x.bottom = if (show_x) element_line(color = "black") else element_blank(),
+      axis.line.x.bottom = element_line(color = "black", linewidth = 0.4),
+      axis.line.y.left   = element_line(color = "black", linewidth = 0.4),
+      axis.ticks.y.left  = element_line(color = "black"),
+      legend.position = "top"
+    )
+}
+
+p_mesic <- make_hab_plot(hab_df, "mesic", show_x = FALSE)
+p_xeric <- make_hab_plot(hab_df, "xeric", show_x = TRUE)
+
+(p_mesic / p_xeric) +
+  plot_layout(guides = "collect") &
+  theme(legend.position = "top")
 
 ## Dans Fig 4 version
 left_join(seed_draws, veg_draws, by=join_by(.draw, habitat, USDA_name)) %>%
