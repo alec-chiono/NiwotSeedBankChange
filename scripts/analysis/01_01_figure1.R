a175c702160c78dc651f2cacf38888221d34899b
Alec Chiono <alec.chiono@colorado.edu>
2026-04-27 13:37:46 -0600

    Collapse figure 1 code into one script

--- scripts/analysis/01_01_figure1.R
+++ scripts/analysis/01_01_figure1.R
@@ -1,13 +1,34 @@
-library(tidyverse, ggbreak, ggthemes, ggimage, patchwork, ggdist)
+# Vizualize raw counts as bar plot
 
-# DATA -------------------------------------------------------------------------
-## Download
+# PACKAGES ----
+library(tidyverse)
+library(ggbreak)
+library(ggthemes)
+library(ggimage)
+library(patchwork)
+
+# DATA ----
+# Source data download function
 source("scripts/source/download_data.R")
 
+# Download and wrangle data
+seed_count_df <-
+  download_data(veg = FALSE)[[1]] %>%
+  filter(substr(USDA_code, 1, 1) != 2 & USDA_code != "CAREX" & USDA_code != "POA") %>% #remove records not identified to species
+  group_by(USDA_name, year) %>%
+  summarize(sum = sum(count), .groups = "drop") %>%
+  mutate(
+    Species = sub("\\s*var\\..*$", "", USDA_name),
+    year=factor(year)
+  )
+  
+# VIZ ----
+# Growth form icon files
 gram <- "images/graminoid.png"
 cush <- "images/cushion.png"
 forb <- "images/forb.png"
 
+# Assign species growth form
 symbol_map <- c(
   "Allium geyeri" = forb,
   "Androsace septentrionalis" = forb,
@@ -33,16 +54,24 @@ symbol_map <- c(
   "Silene acaulis" = cush
 )
 
-# icons under species names
-icon_df <- tibble(
-  Species = names(symbol_map),
-  image = unname(symbol_map),
-  y_icon = -2.2
-) %>%
+# Order Species names by highest->lowest count in 1989
+species_order <-
+  seed_count_df %>%
+  filter(year == 1989) %>%
+  arrange(sum, desc = TRUE) %>%
+  pull(Species)
+
+# Data.frame to plot growth form icons under species names
+icon_df <-
+  tibble(
+    Species = names(symbol_map),
+    image = unname(symbol_map),
+    y_icon = -2.2
+  ) %>%
   mutate(Species = factor(Species, levels = species_order))
 
-# main plot
-p_main <- plot_df %>%
+# Main bar plot
+p_main <- seed_count_df %>%
   mutate(Species = factor(Species, levels = species_order)) %>%
   ggplot(aes(x = Species, y = sum, fill = year, colour = year)) +
   geom_col(position = "dodge") +
@@ -99,7 +128,7 @@ p_main <- plot_df %>%
   ) +
   facet_wrap(~Species, nrow = 1, scales = "free_x")
 
-# custom legend row
+# Custom legend row
 year_leg <- tibble(
   x = c(1.3, 2.4),
   y = 1,
@@ -115,15 +144,6 @@ gf_leg <- tibble(
 )
 
 p_leg <- ggplot() +
-  # annotate(
-  #   "text",
-  #   x = 0.35,
-  #   y = 1,
-  #   label = "Year",
-  #   hjust = 0,
-  #   size = 6,
-  #   family = "serif"
-  # ) +
   geom_tile(
     data = year_leg,
     aes(x = x, y = y, fill = fill),
@@ -138,15 +158,6 @@ p_leg <- ggplot() +
     size = 4.5,
     family = "serif"
   ) +
-  # annotate(
-  #   "text",
-  #   x = 6.7,
-  #   y = 1,
-  #   label = "Growth form",
-  #   hjust = 0,
-  #   size = 6,
-  #   family = "serif"
-  # ) +
   geom_image(
     data = gf_leg,
     aes(x = x, y = y, image = image),
@@ -165,6 +176,6 @@ p_leg <- ggplot() +
   coord_cartesian(xlim = c(0.2, 14.3), ylim = c(0.82, 1.18), clip = "off") +
   theme_void()
 
-f1 <- p_leg / p_main + plot_layout(heights = c(0.12, 1))
+fig1 <- p_leg / p_main + plot_layout(heights = c(0.12, 1))
 
-ggsave("figures/barplot.png", f1, width = 10, height = 5, dpi = 600)
+ggsave("figures/barplot.png", fig1, width = 10, height = 5, dpi = 600)
