58ae94703a2940cc463bcad7ecbf809a807be922
Alec Chiono <alec.chiono@colorado.edu>
2026-04-27 13:37:33 -0600

    Turn download data source into function

--- scripts/source/download_data.R
+++ scripts/source/download_data.R
@@ -1,43 +1,45 @@
-# Script to download data directly from EDI
-# Alec Chiono; alec.chiono@colorado.edu
-
-# PACKAGES ---------------------------------------------------------------------
-librarian::shelf(EDIutils, readr, dplyr)
-
-# DOWNLOAD DATA ----------------------------------------------------------------
-## IDs for the data packages we want to download
-package_id <- c("knb-lter-nwt.333.1", "knb-lter-nwt.93.10")
-
-## Get entity IDs and names from data packages
-entity_df <- rbind(
-  cbind(
-    package_id = package_id[1],
-    read_data_entity_names(packageId = package_id[1])
-  ), #seed bank and co-located veg data
-  cbind(
-    package_id = package_id[2],
-    read_data_entity_names(packageId = package_id[2])
-  ) #longterm veg data
-) %>%
-  filter(entityName != "saddptqd_ancillary_meta.hh.data.txt") #remove metadata file
-
-
-## Download data into list
-data_list <- lapply(
-  1:(nrow(entity_df)), #only get first three files
-  function(i) {
-    read_csv(
-      file = read_data_entity(
-        packageId = entity_df$package_id[i],
-        entityId = entity_df$entityId[i]
-      ),
-      show_col_types = FALSE
+download_data <- function(seed = TRUE, veg = TRUE) {
+  if (seed == FALSE & veg == FALSE) {
+    stop(
+      "seed and veg arguments both FALSE. One must be true to download data."
     )
   }
-)
 
-## Name each dataset in list
-names(data_list) <- entity_df$entityName
+  library(EDIutils)
 
-## Remove unnecessary, intermediate objects
-rm(package_id, entity_df)
+  ## IDs for the data packages we want to download
+  ## [1] seed bank + co-located veg, [2] longterm veg
+  package_id <- c("knb-lter-nwt.333.1", "knb-lter-nwt.93.10")
+
+  ## Select packages based on arguments
+  selected_ids <- package_id[c(seed, veg)]
+
+  ## Get entity IDs and names from selected data packages
+  entity_df <- do.call(
+    rbind,
+    lapply(selected_ids, function(pid) {
+      cbind(package_id = pid, read_data_entity_names(packageId = pid))
+    })
+  )
+  entity_df <- entity_df[
+    entity_df$entityName != "saddptqd_ancillary_meta.hh.data.txt",
+  ]
+
+  ## Download data into named list
+  data_list <- lapply(
+    seq_len(nrow(entity_df)),
+    function(i) {
+      con <- textConnection(
+        rawToChar(read_data_entity(
+          packageId = entity_df$package_id[i],
+          entityId = entity_df$entityId[i]
+        ))
+      )
+      on.exit(close(con))
+      read.csv(con)
+    }
+  )
+  names(data_list) <- entity_df$entityName
+
+  return(data_list)
+}
